e0a3213d28255f80d1e7b73d1ed58360b9e38349
DollySarmiento2024 <dosaqu@alumnatflorida.es>
2025-05-25 23:44:07 +0200

    cambio el username

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
 //DATOS CONEXIÓN A BASE DE DATOS, DEBE COINCIDIR CON EL Dockerfile
 $servername = "db"; //PMA_HOST (en compose.yaml)
-$username = "fernanda"; //MYSQL_USER (usuario base de datos)
-$password = "fernanda"; //MYSQL_PASWORD (contraseña base de datos)
+$username = "root"; //MYSQL_USER (usuario base de datos)
+$password = "root"; //MYSQL_PASWORD (contraseña base de datos)
 $dbname = "cine";  //MYSQL_DATABASE (nombre base de datos)
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
