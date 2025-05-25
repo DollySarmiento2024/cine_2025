9e539b893011614ad2ce03652d0af0b3a837bef9
DollySarmiento2024 <dosaqu@alumnatflorida.es>
2025-05-26 00:41:47 +0200

    vuelvo a copiar archivos

--- php/conexion.php
+++ php/conexion.php
@@ -1,9 +1,9 @@
 <?php
 //DATOS CONEXIÓN A BASE DE DATOS, DEBE COINCIDIR CON EL Dockerfile
-$servername = "db"; //PMA_HOST (en compose.yaml)
-$username = "root"; //MYSQL_USER (usuario base de datos)
-$password = "root"; //MYSQL_PASSWORD (contraseña base de datos)
-$dbname = "cine";  //MYSQL_DATABASE (nombre base de datos)
+$servername = "db";  //PMA_HOST
+$username = "root";  //siempre root
+$password = "root";  //MYSQL_ROOT_PASSWORD
+$dbname = "cine";
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Verificar la conexión
